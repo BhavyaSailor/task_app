36dd9df278eeb0ca3d80ac7404f21ba9dcd4aed1
Bhavya <bhavyas3451@gmail.com>
2026-06-08 16:40:33 +0530

    commit for deployement, connected to backend and vercel SPA rewwrites

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+const API_BASE = import.meta.env.VITE_API_BASE;
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: `${API_BASE}/api`
 })
 
 export const registerUser = async (userData) =>{
